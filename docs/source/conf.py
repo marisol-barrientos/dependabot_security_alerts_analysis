7b002e787f06e2c538860b2a847f5f4030902392
meerschweinchen-seminar <meerschweinchenseminar@gmail.com>
2020-09-07 13:11:34 +0200

    Adding source code inside documentation......

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 MOCK_MODULES = ['pandas', 'influxdb_client', 'influxdb_client.client', 'influxdb_client.client.write_api', 'src', 'semver', 'git']
 for mod_name in MOCK_MODULES:
